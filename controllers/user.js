681d38094ef29fbcdcc2ae86f94d361b81ea7bee
LawJarp-A <prajwalanagani@gmail.com>
2020-12-04 19:44:25 +0530

    Added JWT functionality

---
+++ controllers/user.js
@@ -0,0 +1,56 @@
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+const User = require('../models/User');
+require("dotenv").config();
+
+exports.register = (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    });
+  
+    user.save((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      else {
+          res.status(200).send({user})
+      }
+
+    });
+  };
+
+  exports.signin = (req, res) => {
+    User.findOne({
+      email: req.body.email
+    }).exec((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        if (!user) {
+          return res.status(404).send({ message: "User Not found." });
+        }
+        var passwordIsValid = bcrypt.compareSync(
+          req.body.password,
+          user.password
+        );
+        if (!passwordIsValid) {
+          return res.status(401).send({
+            accessToken: null,
+            message: "Invalid Password!"
+          });
+        }
+        var token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: 86400 // 24 hours
+        });
+        res.status(200).send({
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          accessToken: token
+        });
+      });
+  };
--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const bodyParser = require("body-parser");
 const routes = require("./routes/api");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-
+mongoose.Promise = global.Promise;
 require("dotenv").config();
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 //connect to the database
 mongoose
