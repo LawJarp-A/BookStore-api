417dd9d7e7e284f13a15fd6c0e8df8440398a14d
LawJarp-A <prajwalanagani@gmail.com>
2020-12-09 22:33:44 +0530

    rating update

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String },
+  rating: {type: Number, default: 1},
 });
 
 const User = mongoose.model("User", UserSchema);
