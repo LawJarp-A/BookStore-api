a2ecac783b8030e27a3f232ba250440504f8194a
LawJarp-A <prajwalanagani@gmail.com>
2020-12-09 11:53:51 +0530

    Added more routes

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ const uuid = require("uuid");
 
 const Product = require("../models/Products");
 const User = require('../models/User');
+const Offer = require('../models/Offers')
 
 
 router.post("/register", controller.register);
@@ -24,6 +25,37 @@ router.get("/users", [authenticateJWT], (req, res) => {
   });
 });
 
+router.post("/offers", [authenticateJWT], (req,res) =>{
+  console.log(req.data)
+  const offer = new Offer({
+    seller_email: req.body.seller_email,
+    buyer_email: req.body.buyer_email,
+    product_id: req.body.product_id,
+  });
+  offer.save((err, offer) => {
+    console.log('saved')
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    } else {
+      res.status(200).send({ offer });
+    }
+  });
+})
+
+router.get("/offers", [authenticateJWT], (req, res) => {
+  User.findById(req.user.id,function (err, user) {
+    if (err) return console.error(err);
+    Offer.find({seller_email: user.email}, function(err, offers){
+      console.log(offers)
+      if (err) return console.error(err);
+      res.send(offers)
+
+    })
+  });
+
+});
+
 router.get("/products", (req, res) => {
   Product.find(function (err, products) {
     if (err) return console.error(err);
@@ -32,22 +64,26 @@ router.get("/products", (req, res) => {
 });
 
 router.post("/products", [authenticateJWT], (req, res) => {
-  const product = new Product({
-    product_name: req.body.product_name,
-    product_id: uuid.v4(),
-    price: req.body.price,
-    years_used: req.body.years_used,
-    subject: req.body.subject,
-    seller: req.body.seller,
+  User.findById(req.user.id,function (err, user) {
+    if (err) return console.error(err);
+    const product = new Product({
+      product_name: req.body.product_name,
+      product_id: uuid.v4(),
+      price: req.body.price,
+      years_used: req.body.years_used,
+      subject: req.body.subject,
+      seller: user.email,
+    });
+    product.save((err, product) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      } else {
+        res.status(200).send({ product });
+      }
+    });
   });
-  product.save((err, product) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    } else {
-      res.status(200).send({ product });
-    }
-  });
+  
 });
 
 router.delete("/products", (res, req) => {});
